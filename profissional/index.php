ff671f0bf177488c7fc107a047b2945b698deeaa
João Zuquetto <59144277+joaozcc@users.noreply.github.com>
2024-10-05 18:20:08 -0300

    header e footer add

--- profissional/index.php
+++ profissional/index.php
@@ -11,6 +11,9 @@ include '../config/database.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
+    <?php
+        include '../components/header.php'
+    ?>
     <div class="container mt-5">
         <h2 class="text-center mb-4">Cadastro Profissional da Saúde</h2>
         <form class="row g-3" method="POST" action="insert-usuario.php">
@@ -262,5 +265,8 @@ include '../config/database.php';
             document.getElementById('uf').value = body.uf || '';
         }
     </script>
+    <?php
+        include '../components/footer.php';
+    ?>
 </body>
 </html>
